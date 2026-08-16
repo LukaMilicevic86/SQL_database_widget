4e21b2f90cc70582563db76dcbe296cc62918fe4
Luka Milicevic <lmilicevicmtk@gmail.com>
2026-08-16 21:38:32 +0200

    add insert_data method

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ Config.set("graphics", "width", "340")
 Config.set("graphics", "width", "500")
 
 
-# connect to database function
+# connect to database function, called by the create database function
 def connect_to_database(path):
     try:
         con = sqlite3.connect(path)
@@ -25,7 +25,7 @@ def connect_to_database(path):
     except Exception as e:
         print(e)
 
-# create database table using SQL syntax
+# create database table using SQL syntax; called upon pressing tthe create/enter button in Start widget
 def create_table_products(cursor):
     cursor.execute(
         """
@@ -48,19 +48,19 @@ class MainWid(ScreenManager):
         self.APP_PATH = os.getcwd()
         self.DB_PATH = self.APP_PATH + "/my_database.db"
 
-        # add start widget to screen
+        # add start widget to screen and screen to screen manager
         self.StartWid = StartWid(self)
         ST_wid = Screen(name = "Start")
         ST_wid.add_widget(self.StartWid)
         self.add_widget(ST_wid)
 
-        # add database widget to screen
+        # add database widget to screen and screen to screen manager
         self.DataBaseWid = DataBaseWid(self)
         DB_wid = Screen(name = "Database")
         DB_wid.add_widget(self.DataBaseWid)
         self.add_widget(DB_wid)
 
-        # add insert data widget to screen
+        # add insert data widget to screen and screen to screen manager
         self.InsertDataWid = InsertDataWid(self)
         IDW_wid = Screen(name = "InsertData")
         IDW_wid.add_widget(self.InsertDataWid)
@@ -72,7 +72,7 @@ class MainWid(ScreenManager):
     def go_to_start(self):
         self.current = "Start"
 
-    # open the database widget as current screen
+    # open the database widget as current screenA
     def go_to_database(self):
         self.DataBaseWid.check_memory()
         self.current = "Database"
@@ -80,8 +80,8 @@ class MainWid(ScreenManager):
     #reset everything in insert data widget and open that screen
     def go_to_insertdata(self):
         self.InsertDataWid.clear_widgets() #clears everything from last time
-        IDW_wid = InsertDataWid(self)
-        self.InsertDataWid.add_widget(IDW_wid)
+        NewInsertDataWid = InsertDataWid(self)
+        self.InsertDataWid.add_widget(NewInsertDataWid)
         self.current  = "InsertData"
 
 
@@ -91,7 +91,7 @@ class StartWid(BoxLayout):
         super().__init__()
         self.mainwid = mainwid
 
-    # method to create the database using the path from the mainwid
+    # method to create the database using the path from the mainwid, called when the startwid button is pressed
     def create_database(self):
         connect_to_database(self.mainwid.DB_PATH) 
         self.mainwid.go_to_database()
@@ -104,12 +104,11 @@ class DataBaseWid(BoxLayout):
         self.mainwid = mainwid
     
     # method to delete everything from screen and add everything that is present in the database
-
     def check_memory(self):
         self.ids.container.clear_widgets()
 
-        wid = NewDataButton(self.mainwid) 
-        self.ids.container.add_widget(wid)
+        NDBwid = NewDataButton(self.mainwid) 
+        self.ids.container.add_widget(NDBwid)
 
 
 
@@ -128,9 +127,37 @@ class InsertDataWid(BoxLayout):
         super().__init__()
         self.mainwid = mainwid
 
-    #methhod to fill in the data windows for columns in the table
+    #method to fill in the data windows for columns in the table
     def insert_data(self):
-        print("insert data")
+        con = sqlite3.connect(self.mainwid.DB_PATH)
+        cursor = con.cursor()
+
+        d1 = self.ids.ti_name.text
+        d2 = self.ids.ti_code.text
+        d3 = self.ids.ti_price.text
+        d4 = self.ids.ti_quantity.text
+        
+
+        a1 = (d1, d2, d3, d4)
+
+        s1 = 'INSERT INTO products(Name,Code,Price,Quantity)'
+        s2 = 'VALUES("%s", "%s", %s, %s)' %a1 # %s are placeholders for strings and numbers
+
+        try:
+            cursor.execute(s1 + " " + s2)
+            con.commit()
+            con.close()
+            self.mainwid.go_to_database()
+
+        except Exception as e:
+            print(str(e))
+            #message = self.mainwid.Popup.ids.message
+            #self.mainwid.Popup.open()
+            #self.mainwid.Popup.title = "Error!"
+            #if "" in a1:
+             #   message.text = "One or more fields empty"
+            #else:
+             #   message.text = str(e)
 
     #return to the database widget upon pressing Exit
     def back_to_dbw(self):
